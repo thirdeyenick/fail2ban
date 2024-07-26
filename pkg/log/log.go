042505eeb08d20157463c9a5df6dfb449774abd3
Sebastian Nickel <nick@nine.ch>
2024-07-26 11:56:41 +0200

    fix logging

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,21 +1,15 @@
-//go:build DEBUG
-
 // Package log contains the logger mechanism for the plugin.
-// Noop logger for non-debug builds.
 package log
 
 import (
 	"log"
+	"os"
 )
 
 type Logger struct {
 	*log.Logger
 }
 
-func New(string) Logger {
-	return Logger{}
+func New(prefix string) Logger {
+	return Logger{log.New(os.Stdout, prefix+": ", log.Ldate|log.Ltime|log.Lshortfile)}
 }
-
-func (l Logger) Printf(format string, v ...interface{}) {}
-
-func (l Logger) Println(v ...interface{}) {}
